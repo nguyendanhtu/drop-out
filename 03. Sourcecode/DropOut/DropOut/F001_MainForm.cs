71c0f89c002cc0ace1b12568ebe2ac1ca82f814a
vunb <vunb@bkindex.com>
2011-09-15 13:01:28 +0000

    Khởi tạo solution: DropOut - Dựng khung Form chính - Tạo User Control: ResizeableSplitter - Tạo User Control: ControlTitle - Đã sắp xếp được khung 2 tab: Analysis, Preprocesssing. - Hoàn tất được 70% Main Form

---
+++ 03. Sourcecode/DropOut/DropOut/F001_MainForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DropOut
+{
+    public partial class F001_MainForm : Form
+    {
+        public F001_MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void statusbarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trainingLogToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
