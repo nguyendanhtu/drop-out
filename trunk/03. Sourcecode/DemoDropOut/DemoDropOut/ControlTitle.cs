bdae8ffc9fafe7c3c0b6f30b22e6a29f7732788e
vunb@bkindex.com <vunb@bkindex.com@a212c5ba-5817-de58-74f4-9c5d418d63a2>
2011-09-23 11:04:53 +0000

    Xây dựng chương trình DemoDropOut Kiểm thử dữ liệu

---
+++ trunk/03. Sourcecode/DemoDropOut/DemoDropOut/ControlTitle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace DropOut.Uc
+{
+    public class ControlTitle : UserControl
+    {
+        private Label lbTitle;
+    
+        private void InitializeComponent()
+        {
+            this.lbTitle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.lbTitle.AutoSize = true;
+            this.lbTitle.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold);
+            this.lbTitle.ForeColor = System.Drawing.Color.White;
+            this.lbTitle.Location = new System.Drawing.Point(3, 3);
+            this.lbTitle.Name = "lbTitle";
+            this.lbTitle.Size = new System.Drawing.Size(77, 13);
+            this.lbTitle.TabIndex = 0;
+            this.lbTitle.Text = "Title Text";
+            // 
+            // ControlHeader
+            // 
+            this.BackColor = System.Drawing.Color.Gray;
+            this.Controls.Add(this.lbTitle);
+            this.Name = "ControlTitle";
+            this.Size = new System.Drawing.Size(430, 20);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        public ControlTitle()
+        {
+            InitializeComponent();
+            Dock = DockStyle.Top;
+        }
+
+        /// <summary>
+        /// The delay in millisenconds between animation steps
+        /// </summary>
+        [Bindable(true), DefaultValue("Title Text"), Description("Title for control.")]
+        public string Title
+        {
+            get { return lbTitle.Text;  }
+            set { lbTitle.Text = value; }
+        }
+    }
+}
---
+++ trunk/03. Sourcecode/DemoDropOut/DemoDropOut/Objects/ColumnDetails.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoDropOut.Objects
+{
+    public class ColumnDetails
+    {
+        public ColumnType Type;
+        public ColumnFormat Format;
+        public int EncodedColumnInto;
+        public double MinValue = double.MaxValue;
+        public double MaxValue = double.MinValue;
+
+        public ColumnDetails()
+        {
+            Type = ColumnType.Input;
+            Format = ColumnFormat.Numerical;
+            EncodedColumnInto = 1;
+        }
+
+        public ColumnDetails(ColumnType type, ColumnFormat format)
+        {
+            Type = type;
+            Format = format;
+        }
+
+        public ColumnDetails(ColumnType type, ColumnFormat format, int encodeColNumber)
+            : this(type, format)
+        {
+            EncodedColumnInto = encodeColNumber;
+        }
+    }
+
+    public enum ColumnFormat
+    {
+        Numerical,
+        Categorical,
+        Date
+    }
+
+    public enum ColumnType
+    {
+        Input,
+        Ouput
+    }
+}
